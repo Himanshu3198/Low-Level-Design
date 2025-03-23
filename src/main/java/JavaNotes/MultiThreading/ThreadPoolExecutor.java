96d1f3521badef23e73d3a12d5ac3ba09b7f6077
Himanshu <himanshu3198live@gmail.com>
2025-03-24 01:18:39 +0530

    Multithreading Basic

--- src/main/java/JavaNotes/MultiThreading/ThreadPoolExecutor.java
+++ src/main/java/JavaNotes/MultiThreading/ThreadPoolExecutor.java
@@ -1,4 +1,35 @@
 package JavaNotes.MultiThreading;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class workerThread implements Runnable{
+    String message;
+    workerThread(String message){
+        this.message = message;
+    }
+    public void run(){
+
+        try{
+            System.out.println("Thread->"+Thread.currentThread().getName()+", "+message);
+            Thread.sleep(100);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+//        System.out.println("Thread->"+Thread.currentThread().getName());
+    }
+}
 public class ThreadPoolExecutor {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        for(int i=1;i<=5;i++){
+            Runnable worker = new workerThread("Task"+i);
+            executor.execute(worker);
+        }
+        executor.shutdown();
+
+    }
+
+
 }
